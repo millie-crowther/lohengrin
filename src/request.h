61ede62334d677b0f7a675d21c3825a356f6e292
millie-crowther <amelia.g.crowther@gmail.com>
2023-10-23 10:14:58 +0100

    Update request.h

--- src/request.h
+++ src/request.h
@@ -1,6 +1,15 @@
 #ifndef LOHENGRIN_REQUEST_H
 #define LOHENGRIN_REQUEST_H
 
+typedef enum lh_http_method {
+    LH_HTTP_METHOD_GET,
+    LH_HTTP_METHOD_POST,
+    LH_HTTP_METHOD_PUT,
+    LH_HTTP_METHOD_PATCH,
+    LH_HTTP_METHOD_HEAD,
+    LH_HTTP_METHOD_DELETE
+} lh_http_method;
+
 typedef struct lh_request {
 } lh_request;
 

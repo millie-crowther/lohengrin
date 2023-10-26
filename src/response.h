572bf4adc634f9b1661e1297032cf20dbc2c7836
millie-crowther <amelia.g.crowther@gmail.com>
2023-10-26 21:29:02 +0100

    Create response.h

---
+++ src/response.h
@@ -0,0 +1,13 @@
+#ifndef LOHENGRIN_RESPONSE_H
+#define LOHENGRIN_RESPONSE_H
+
+#include <stdint.h>
+
+typedef struct lh_response {
+    uint16_t code;
+    const char * body;
+} lh_response;
+
+const char * lh_response_get_name_for_code(uint16_t response_code)
+
+#endif

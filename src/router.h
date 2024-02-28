d041775c616fc9f4f2d7f3adb0971d5046000f91
millie-crowther <amelia.g.crowther@gmail.com>
2024-02-28 16:22:45 +0000

    Update router.h

--- src/router.h
+++ src/router.h
@@ -7,24 +7,12 @@
 #include "request.h"
 #include "response.h"
 
-typedef struct lh_router_response {
-    bool path_match_found;
-    bool match_found;
-    lh_response response;
-} lh_router_response;
-
-typedef struct lh_route {
-    lh_http_method method;
-    const char *path;
-    lh_route_handler route_handler;
-} lh_route;
-
 typedef struct lh_router {
-    lh_route *routes;
-    uint32_t route_count;
+    lh_url *urls;
+    uint32_t url_count;
 } lh_router;
 
-void lh_router_register_route(lh_router router[static 1], lh_route route);
+void lh_router_register_route(lh_router router[static 1], lh_url url);
 lh_router_response router_handle_request(lh_router router[static 1], const lh_request request[static 1], lh_context context[static 1]);
 
 #endif
